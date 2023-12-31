b42b77701e9c29bb3c2091f75a97ab908bed0a85
Gabriel <60631953+wolerdev@users.noreply.github.com>
2023-12-31 22:46:24 +0000

    [fix] Update auth and orders DTOs

--- apps/api-gateway/src/auth/dto/create-auth.dto.ts
+++ apps/api-gateway/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAuthDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    name?: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+}
--- apps/api-gateway/src/products/dto/create-product.dto.ts
+++ apps/api-gateway/src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateProductDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    price: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    shopId?: number;
+}
